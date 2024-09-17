aeb616d64d38037409908249e2090eac6eab72f7
SantiagoAlegre <alegresantisp@gmail.com>
2024-09-17 16:59:49 -0300

    nuevasFuncionalidades

--- src/components/mycomponent/mycomponent.tsx
+++ src/components/mycomponent/mycomponent.tsx
@@ -6,16 +6,57 @@ import { IoLogoCss3 } from 'react-icons/io'
 import { MdEmail } from 'react-icons/md'
 import Image from 'next/image'
 
+
 function MyComponent() {
   const [isProjectExpanded, setIsProjectExpanded] = useState(false)
+  const [isSpanish, setIsSpanish] = useState(true)
+
+  const toggleLanguage = () => {
+    setIsSpanish(!isSpanish)
+  }
+
+  const content = {
+    es: {
+      home: 'Inicio',
+      projects: 'Proyectos',
+      about: 'Sobre mí',
+      contact: 'Contacto',
+      myProjects: 'Mis Proyectos',
+      espec: 'Desarrollador Full Stack especializado en Front End',
+      ribuzz: 'Ribuzz',
+      viewProject: 'Ver Proyecto',
+      aboutMeTitle: 'Sobre mí',
+      aboutMe: `Soy una persona curiosa y apasionada por el aprendizaje continuo. Mi formación en counselling me ha permitido desarrollar habilidades como la escucha activa, la empatía y un profundo autoconocimiento, que han sido fundamentales para mi crecimiento personal y profesional.`,
+      aboutMeMore: `Mi curiosidad y deseo de crear me llevaron a estudiar programación, especializándome en el desarrollo front-end. Mi objetivo es seguir creciendo en este apasionante mundo del desarrollo web.`,
+      mySocialMedia: 'Mis Redes Sociales',
+      footer: 'El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. Si amas lo que haces, tendrás éxito. - Albert Schweitzer'
+    },
+    en: {
+      home: 'Home',
+      projects: 'Projects',
+      about: 'About',
+      contact: 'Contact',
+      myProjects: 'My Projects',
+      espec: 'Full Stack Developer specialized in Front End',
+      ribuzz: 'Ribuzz',
+      viewProject: 'View Project',
+      aboutMeTitle: 'About Me',
+      aboutMe: `I am a curious person passionate about continuous learning. My background in counselling has allowed me to develop skills such as active listening, empathy, and deep self-knowledge, essential for my personal and professional growth.`,
+      aboutMeMore: `My curiosity and desire to create led me to study programming, specializing in front-end development. My goal is to keep growing in this exciting world of web development.`,
+      mySocialMedia: 'My Social Media',
+      footer: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful. - Albert Schweitzer'
+    }
+  }
+
+  const t = isSpanish ? content.es : content.en
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-gray-800 to-purple-900 text-white">
       {/* Navbar */}
-      <nav className="p-4">
+      <nav className="px-4 py-1 mt-2 bg-gradient-to-br from-purple-900  to-purple-900 inline-block rounded-xl shadow-lg sticky top-0 left-1/2 transform -translate-x-1/2">
         <ul className="flex justify-center space-x-8">
-          {['Home', 'Projects', 'About', 'Contact'].map((item) => (
-            <li key={item} className="relative group">
+          {[t.home, t.projects, t.about, t.contact].map((item, index) => (
+            <li key={index} className="relative group">
               <a href={`#${item.toLowerCase()}`} className="text-xl hover:text-purple-300 transition-colors duration-300">
                 {item}
                 <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-purple-300 transition-all duration-300 group-hover:w-full"></span>
@@ -25,59 +66,78 @@ function MyComponent() {
         </ul>
       </nav>
 
-      {/* Hero Section */}
-      <section id="home" className="container mx-auto px-4 py-16 flex flex-col md:flex-row items-center">
-        <Image 
-          src="/assets/imagen1.jfif"  
-          alt="Santiago Alegre" 
-          width={300} 
-          height={300} 
-          className="rounded-full w-64 h-64 object-cover mb-8 md:mb-0 md:mr-8"
-        />
-        <div>
-          <h1 className="text-4xl font-bold mb-2">Santiago Alegre</h1>
-          <p className="text-xl mb-4">Full Stack Developer specialized in Front End</p>
-          <div className="flex space-x-4">
-            <SiNextdotjs size={24} />
-            <FaReact size={24} />
-            <SiTailwindcss size={24} />
-            <IoLogoCss3 size={24} />
-            <FaGithub size={24} />
+      {/* Language Toggle */}
+      <div className="flex justify-center my-4">
+        <button onClick={toggleLanguage} className="px-1 py-0.5 bg-purple-500 rounded-full hover:bg-purple-600 transition-colors duration-300">
+          {isSpanish ? 'En' : 'Es'}
+        </button>
+      </div>
+
+        {/* Hero Section */}
+        <section id="home" className="flex items-center justify-center min-h-screen px-4 py-8">
+          <div className="flex items-center max-w-5xl mx-auto">
+            <div className="flex-shrink-0 mr-8">
+              <Image 
+                src="/assets/imagen1.jfif"  
+                alt="Santiago Alegre" 
+                width={300} 
+                height={300} 
+                className="rounded-full w-64 h-64 object-cover"
+              />
+            </div>
+            <div className="text-left">
+              <h1 className="text-4xl font-bold mb-2">Santiago Alegre</h1>
+              <p className="text-xl mb-4">{t.espec}</p>
+              <div className="flex space-x-4 ">
+                <SiNextdotjs size={24} />
+                <FaReact size={24} />
+                <SiTailwindcss size={24} />
+                <IoLogoCss3 size={24} />
+                <FaGithub size={24} />
+              </div>
+            </div>
           </div>
+        </section>
+
+
+          {/* Projects Section */}
+      <section id="projects" className="container mx-auto px-4 py-16">
+        <h2 className="text-3xl font-bold mb-8">{t.myProjects}</h2>
+        <div className="flex flex-wrap gap-4">
+          <div className="bg-gray-700 rounded-lg p-4 cursor-pointer w-64 h-64 flex flex-col">
+            <Image
+              src="/assets/ribuzz.webp"
+              alt="Ribuzz Project"
+              width={150}
+              height={150}
+              className="rounded object-cover mb-4 w-full h-full"
+            />
+            <div className="flex items-center justify-between mb-2">
+              <h3 className="text-xl font-semibold">{t.ribuzz}</h3>
+              <a href="https://ribuzz.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-purple-300 hover:underline">{t.viewProject}</a>
+            </div>
+            {isProjectExpanded && (
+              <p className="mt-4 text-gray-300">
+                Ribuzz es una plataforma de marketplace y networking diseñada para emprendedores e inversores.
+              </p>
+            )}
+          </div>
+          {/* Agrega más componentes de proyectos aquí */}
         </div>
       </section>
 
-      {/* Projects Section */}
-      <section id="projects" className="container mx-auto px-4 py-16">
-        <h2 className="text-3xl font-bold mb-8">My Projects</h2>
-        <div className="bg-gray-700 rounded-lg p-4 cursor-pointer" onClick={() => setIsProjectExpanded(!isProjectExpanded)}>
-          <h3 className="text-xl font-semibold mb-2">Ribuzz</h3>
-          <a href="https://ribuzz.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-purple-300 hover:underline">View Project</a>
-          {isProjectExpanded && (
-            <p className="mt-4 text-gray-300">
-              Ribuzz is a web application that... (Add your project description here)
-            </p>
-          )}
-        </div>
-      </section>
+
 
       {/* About Me Section */}
       <section id="about" className="container mx-auto px-4 py-16">
-        <h2 className="text-3xl font-bold mb-8">About Me</h2>
-        <p className="text-gray-300 leading-relaxed">
-          Soy una persona curiosa y apasionada por el aprendizaje continuo. Mi formación en counselling me ha permitido desarrollar habilidades como la escucha activa, la empatía y un profundo autoconocimiento, que han sido fundamentales para mi crecimiento personal y profesional. A lo largo de este camino, descubrí mis fortalezas y las áreas donde puedo mejorar, lo que me llevó a explorar nuevas formas de contribuir de manera significativa.
-        </p>
-        <p className="text-gray-300 leading-relaxed mt-4">
-          Mi curiosidad y deseo de crear me llevaron a estudiar programación, especializándome en el desarrollo front-end. En este campo, encuentro la perfecta combinación entre creatividad y técnica, lo que me permite diseñar y desarrollar soluciones digitales. Creo firmemente en el poder del desarrollo web para impulsar marcas y emprendimientos.
-        </p>
-        <p className="text-gray-300 leading-relaxed mt-4">
-          Mi objetivo es seguir creciendo en este apasionante mundo del desarrollo web, aportando mis habilidades y experiencia para colaborar en proyectos que generen valor y aprendiendo junto a otros profesionales comprometidos.
-        </p>
+        <h2 className="text-3xl font-bold mb-8">{t.aboutMeTitle}</h2>
+        <p className="text-gray-300 leading-relaxed">{t.aboutMe}</p>
+        <p className="text-gray-300 leading-relaxed mt-4">{t.aboutMeMore}</p>
       </section>
 
       {/* Social Media Section */}
       <section id="contact" className="container mx-auto px-4 py-16">
-        <h2 className="text-3xl font-bold mb-8">My Social Media</h2>
+        <h2 className="text-3xl font-bold mb-8">{t.mySocialMedia}</h2>
         <div className="flex justify-center space-x-8">
           <a href="https://github.com/alegresantisp" target="_blank" rel="noopener noreferrer" className="text-4xl hover:text-purple-300 transition-colors duration-300">
             <FaGithub />
@@ -97,7 +157,7 @@ function MyComponent() {
       {/* Footer */}
       <footer className="bg-gray-800 text-center py-8">
         <p className="text-gray-300 italic">
-          El éxito no es la clave de la felicidad. La felicidad es la clave del éxito. Si amas lo que haces, tendrás éxito. - Albert Schweitzer
+          {t.footer}
         </p>
       </footer>
     </div>
